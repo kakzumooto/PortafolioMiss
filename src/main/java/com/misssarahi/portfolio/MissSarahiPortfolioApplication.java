05260eb329ecbf0bd5c9123b4bb38403f8ee33c3
kakzumooto <kakzumooto22@gmail.com>
2025-08-05 10:55:22 -0600

    Creacion de plantilla para portafolio web

---
+++ src/main/java/com/misssarahi/portfolio/MissSarahiPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.misssarahi.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissSarahiPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MissSarahiPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misssarahi/portfolio/MissSarahiPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misssarahi.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissSarahiPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
